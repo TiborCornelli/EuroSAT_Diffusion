0fa6d4285e2ba0b36e500e3042560e1eb112ad3b
TiborCornelli <tibor.cornelli@t-online.de>
2026-04-22 19:17:05 +0200

    New results from transformer architecture

--- src/DiffusionModel.py
+++ src/DiffusionModel.py
@@ -140,6 +140,31 @@ class Diffusion:
                 x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
         return x
 
+class VisualDiffusion(Diffusion):
+    def sample_evolution(self, model, n=1, steps_to_save=[999, 750, 500, 250, 0]):
+        model.eval()
+        results = {}
+        device = next(model.parameters()).device
+        
+        with torch.no_grad():
+            x = torch.randn((n, 3, 64, 64)).to(device)
+            for i in tqdm(reversed(range(0, self.steps)), total=self.steps, leave=False):
+                t = (torch.ones(n) * i).long().to(device)
+                predicted_noise = model(x, t)
+                
+                alpha = self.alpha[t][:, None, None, None]
+                alpha_hat = self.alpha_hat[t][:, None, None, None]
+                beta = self.beta[t][:, None, None, None]
+                
+                noise = torch.randn_like(x) if i > 0 else torch.zeros_like(x)
+                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
+                
+                if i in steps_to_save:
+                    img = (x.clone().clamp(-1, 1) + 1) / 2
+                    results[i] = img.cpu()
+        
+        return [results[s] for s in steps_to_save]
+
 def get_eurosat_loader(data_dir, batch_size=64):
     transform = transforms.Compose([
         transforms.Resize((64, 64)),
